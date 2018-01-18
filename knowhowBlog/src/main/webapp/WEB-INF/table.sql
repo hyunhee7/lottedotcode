d72bdf025bdc60bdb9a8ea934d6b8e49e8fdb552
hyunhee7 <hyunhi7@naver.com>
2018-01-18 17:41:34 +0900

    added homeController

---
+++ knowhowBlog/src/main/webapp/WEB-INF/table.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE MEMBERS (
+	id 			VARCHAR(50) PRIMARY KEY, /* 아이디 */
+	pwd			VARCHAR(50) NOT NULL, 	 /* 비밀번호 */
+	name		VARCHAR(50) NOT NULL,	 /* 이름 */
+	image		BLOB					 /* 이미지 */
+)
