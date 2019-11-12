04440516782f91d15269bcc196e210c0934c8ff0
Hyunhee Kim <hyunhi7@naver.com>
2019-11-13 02:34:21 +0900

    의존성 문제 해결

--- knowhowBlog/src/main/java/com/mycompany/myapp/service/ProjTimelineService.java
+++ knowhowBlog/src/main/java/com/mycompany/myapp/service/ProjTimelineService.java
@@ -9,11 +9,11 @@ import com.mycompany.myapp.dto.ProjTimelineDto;
 
 /* 프로젝트 타임라인 Service */
 public interface ProjTimelineService {
-	public int insert(ProjTimelineDto dto,HttpServletRequest request);		/* 포스트 등록 */
-	ModelAndView list(int num);												/* 포스트 리스트  */
-	public ModelAndView detail(ProjTimelineDto dto);						/* 포스트 상세보기 */
-	public ModelAndView getFile(ProjTimelineDto dtoNum);					/* 포스트 파일 다운로드 준비 */
-	public void update(ProjTimelineDto dto, HttpServletRequest request);	/* 포스트 수정 */
+	public int insert(ProjTimelineDto dto);									/* 포스트 등록 */
+//	public ProjTimelineDto list(int num);									/* 포스트 리스트  */
+	public ProjTimelineDto detail(ProjTimelineDto dto);						/* 포스트 상세보기 */
+	public ProjTimelineDto getFile(ProjTimelineDto dtoNum);					/* 포스트 파일 다운로드 준비 */
+	public void update(ProjTimelineDto dto);	/* 포스트 수정 */
 	public void delete(ProjTimelineDto dto);								/* 포스트 삭제 */
 	public void cmtInsert(ProjPostCommentDto dto);							/* 포스트 댓글 등록 */
 }
