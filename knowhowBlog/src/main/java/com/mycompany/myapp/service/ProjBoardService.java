6454b0d7142f76ae44867e4bf91ba00b56248627
Hyunhee Kim <hyunhi7@naver.com>
2019-11-13 02:36:57 +0900

    의존성 문제 해결

--- knowhowBlog/src/main/java/com/mycompany/myapp/service/ProjBoardService.java
+++ knowhowBlog/src/main/java/com/mycompany/myapp/service/ProjBoardService.java
@@ -9,10 +9,10 @@ import org.springframework.web.servlet.ModelAndView;
 import com.mycompany.myapp.dto.ProjBoardDto;
 /* 프로젝트 보드 Service */
 public interface ProjBoardService {	
-	public void insert(ProjBoardDto dto,HttpServletRequest request);		/* 프로젝트 등록 */
-	public ModelAndView list();												/* 프로젝트 리스트 */
+	public void insert(ProjBoardDto dto);									/* 프로젝트 등록 */
+	public List<ProjBoardDto> list();										/* 프로젝트 리스트 */
 	public List<ProjBoardDto> recentList();									/* 최근 프로젝트 리스트 */
-	public ModelAndView detail(int num);									/* 프로젝트 상세보기 */
-	public void update(ProjBoardDto dto, HttpServletRequest request);		/* 프로젝트 수정 */
+	public ProjBoardDto detail(int num);									/* 프로젝트 상세보기 */
+	public void update(ProjBoardDto dto);									/* 프로젝트 수정 */
 	public void delete(int proj_num);										/* 프로젝트 삭제 */
 }
