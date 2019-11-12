0fabd2fd4ea40a2b5c132f10c35e25e0eaa8af3c
Hyunhee Kim <hyunhi7@naver.com>
2019-11-13 02:46:39 +0900

    의존성 문제 해결

--- knowhowBlog/src/main/java/com/mycompany/myapp/service/KnowhowService.java
+++ knowhowBlog/src/main/java/com/mycompany/myapp/service/KnowhowService.java
@@ -10,12 +10,12 @@ import com.mycompany.myapp.dto.KnowhowCommentDto;
 import com.mycompany.myapp.dto.KnowhowDto;
 /* 노하우 Service */
 public interface KnowhowService {
-	public ModelAndView list();											/* 노하우 리스트 */
-	public ModelAndView Searchlist(String tag_name);					/* 노하우 태그별 검색 */
-	public int insert(KnowhowDto dto,HttpServletRequest request);		/* 노하우 등록 */
-	public ModelAndView detail(KnowhowDto dtoNum);						/* 노하우 상세보기 */
-	public ModelAndView getFile(KnowhowDto dtoNum);						/* 노하우 첨부파일 다운로드 준비 */
-	public void update(KnowhowDto dto, HttpServletRequest request);		/* 노하우 수정 */
+	public List<KnowhowDto> list();											/* 노하우 리스트 */
+	public List<KnowhowDto> Searchlist(String tag_name);					/* 노하우 태그별 검색 */
+	public int insert(KnowhowDto dto);									/* 노하우 등록 */
+	public KnowhowDto detail(KnowhowDto dtoNum);						/* 노하우 상세보기 */
+	public KnowhowDto getFile(KnowhowDto dtoNum);						/* 노하우 첨부파일 다운로드 준비 */
+	public void update(KnowhowDto dto);		/* 노하우 수정 */
 	public void delete(int kh_num);										/* 노하우 삭제 */
 	public void cmtInsert(KnowhowCommentDto dto);						/* 노하우 댓글 등록 */
 	public List<KnowhowDto> recentList();								/* 노하우 최근 게시물  */
